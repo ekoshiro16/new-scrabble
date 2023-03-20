be729f7bed6b0f06acd084bf1fa262dae2360c63
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-19 19:31:31 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+You are a game developer, and you think you have come up with the next best word game to rival the famous game, Scrabble. Yours, though, will be available to play online! You've just got to come up with the code to build it into being...
+
+In your word game, consonants (any letter besides the 5 main vowels) have a value equivalent to their position in the English alphabet. For example, the letter "b", being the 2nd letter in the alphabet, has a 2 points value in your game, while the letter "z", being the last consonant in the alphabet, has a 26 points value. 
+
+However, vowels have zero point value in your game. 
+
+Write an algorithm that accepts a string argument, representing a given word "played" in your game. Your algorithm should return an integer representing the total point value of the given word.
+
+For example, 
+If you were given a string like "apple"...
+Your algorithm would return 44, because the consonant "p" is the 16th letter in the alphabet, and the consonant "l" is the 12th letter in the alphabet. 
+So 16 + 16 + 12 = 44. 
+Note that the 2 vowels are not considered in the point value.
+
+If you were given a string like "as"...
+Your algorithm would return 19, since the only consonant in the word is "s", which is the 19th letter in the alphabet. 
+*/
+
+// Write your code below: 
